6d35ade2a780589afa52bceee4b820666e881bd8
FlexibleToast <JMcDade42@gmail.com>
2023-04-29 08:12:14 -0500

    Redo remove_snapper_snapshots and rebalance_drives

--- rebalance_drives.sh
+++ rebalance_drives.sh
@@ -1,44 +1,127 @@
 #!/bin/bash
 set -eu
 
-configs=$(ls -1 /etc/snapper/configs | grep disk)
-percent=0.25
-pool=/srv/mergerfs/backend
-tools=/root/mergerfs-tools/src
+##################### FUNCTION DEFINITIONS #########################
+parse_args() {
+  # Set default values for options
+  configs=(/etc/snapper/configs/disk*)
+  percent=0.25
+  pool="/srv/mergerfs/backend"
+  tools="/root/mergerfs-tools/src"
 
-echo "Stopping snapper from making timeline snapshots"
-pushd /etc/snapper/configs
-sed -i "s/TIMELINE_CREATE=\"yes\"/TIMELINE_CREATE=\"no\"/g" ${configs}
+  # Use getopt to parse command line arguments
+  ARGS=$(getopt -o hc:p:l:t: --long help,config:,percent:,pool:,tools: --name "$(basename "$0")" -- "$@")
+  # Get the command line arguments and assign them to variables according to their corresponding flags
+  # h: -> means the -h flag needs an argument
+  # l -> means the -l flag doesn't have an argument 
+  # "hc:p:l:t:" -> all the possible flags
 
-echo "Dedup mergerfs"
-dedup_result=$(${tools}/mergerfs.dedup -v -D .snapshots -D snapraid --dedup=newest ${pool} | tee /dev/tty)
-savings=$(echo ${dedup_result} | tail -n 1 | cut -d ' ' -f 4)
-if [[ $savings != "0.0B" ]]
-  then
-  read -p "Perform dedup? [y/N] " dedup
-  [[ ${dedup} =~ ^(y|Y|yes|Yes)$ ]] && ${tools}/mergerfs.dedup -e -D .snapshots -D snapraid --dedup=newest ${pool}
-fi
+  eval set -- "${ARGS}"  # This sets positional parameters to the arguments that were used in getopt
 
-echo "Remove current snapper snapshots"
-for config in ${configs}
-  do
-  mount=$(lsblk -o label,mountpoint | grep ${config} | awk '{print $2}')
-  snapshots=$(ls -1 ${mount}/.snapshots)
-  for snapshot in ${snapshots}
-    do
-    snapper -c ${config} delete ${snapshot} 2> /dev/null || echo "Config: ${config}; Snapshot: ${snapshot}; Mount: ${mount} ecnountered an error"
+  while true; do  # Start loop to go through all command line arguments
+    case "${1}" in  
+      -h|--help)  # Show usage information and exit
+        echo "Usage: $(basename "$0") [-c configs] [-p percent] [-l pool] [-t tools]"
+        exit 0
+        ;;
+      -c|--configs)  # Change default configuration files to be used
+        configs=(${2})
+        shift 2
+        ;;
+      -p|--percent)  # Change rebalance percentage
+        percent="${2}"
+        shift 2
+        ;;
+      -l|--pool)  # Change path to mergerfs mount point
+        pool="${2}"
+        shift 2
+        ;;
+      -t|--tools)  # Change path to mergerfs tool binaries
+        tools="${2}"
+        shift 2
+        ;;
+      --)  # End of arguments
+        shift
+        break
+        ;;
+      *)  # Error if an invalid argument is received
+        echo "Error: Invalid option ${1}"
+        exit 1
+        ;;
+    esac
   done
-done
+}
 
-echo "Cleanup old snapraid data"
-find /srv/ -type f -name snapraid.content -delete
+disable_snapper() {
+  local configs=("$@")
+  printf 'Disabling timeline snapshots for %s\n' "${configs[@]}"
+  sed -i "s/TIMELINE_CREATE=\"yes\"/TIMELINE_CREATE=\"no\"/g" "${configs[@]}"
+}
 
-echo "Rebalance data"
-${tools}/mergerfs.balance -p ${percent} ${pool}
+enable_snapper() {
+  local configs=("$@")
+  printf 'Re-enable timeline snapshots for %s\n' "${configs[@]}"
+  sed -i "s/TIMELINE_CREATE=\"no\"/TIMELINE_CREATE=\"yes\"/g" "${configs[@]}"
+}
 
-echo "Sync snapraid"
-snapraid-btrfs sync
+remove_snapper_snapshots() {
+  local configs=("$@")
+  for config in "${configs[@]}"; do
+    label=$(basename "$config")
+    mountpoint=$(lsblk -o NAME,LABEL,MOUNTPOINT | awk -v label="$label" '$2 == label { print $3 }')
+    printf 'Removing current snapper snapshots for config %s...\n' "$label"
+    while IFS= read -r -d '' snapshot; do
+      if ! snapper -c "$label" delete "$snapshot"; then
+        printf 'Error: could not delete snapshot %s\n' "$snapshot" >&2
+      fi
+    done < <(find "$mountpoint/.snapshots" -mindepth 1 -maxdepth 1 -type d -printf '%f\0' 2>/dev/null || true)
+  done
+}
 
-echo "Re-enable timeline snapshots"
-sed -i "s/TIMELINE_CREATE=\"no\"/TIMELINE_CREATE=\"yes\"/g" $(ls -1 /etc/snapper/configs | grep disk)
-popd
+dedup_mergerfs() {
+  local tools="$1"
+  local pool="$2"
+
+  printf 'Dedup mergerfs\n'
+  dedup_result=$("${tools}/mergerfs.dedup" -v -D .snapshots -D snapraid --dedup=newest "${pool}" | tee /dev/tty)
+  savings=$(echo "${dedup_result}" | tail -n 1 | awk '{print $4}')
+  if [ "${savings%.*}" -gt 0 ]; then
+    read -p "Perform dedup? [y/N] " dedup
+    [[ "${dedup}" =~ ^(y|Y|yes|Yes)$ ]] && "${tools}/mergerfs.dedup" -e -D .snapshots -D snapraid --dedup=newest "${pool}"
+  fi
+}
+
+rebalance_data() {
+  local tools="$1"
+  local percent="$2"
+  local pool="$3"
+
+  printf 'Rebalance data\n'
+  "${tools}/mergerfs.balance" -p "${percent}" "${pool}"
+}
+
+# Function to remove old SnapRAID data files
+remove_snapraid_data() {
+  printf 'Cleanup old snapraid data\n'
+  find /srv/ -type f -name snapraid.content -delete
+}
+
+# Function to sync SnapRAID parity
+sync_snapraid_parity() {
+  printf 'Sync snapraid\n'
+  snapraid-btrfs sync
+}
+
+##################### MAIN SCRIPT ###################################
+
+# Call function to parse command line arguments
+parse_args "$@"
+
+# Call each function separately
+disable_snapper ${configs[@]}
+dedup_mergerfs ${tools} ${pool}
+remove_snapper_snapshots ${configs[@]}
+remove_snapraid_data
+rebalance_data ${tools} ${percent} ${pool}
+sync_snapraid_parity
+enable_snapper ${configs[@]}
--- remove_snapper_snapshots.sh
+++ remove_snapper_snapshots.sh
@@ -1,15 +1,82 @@
 #!/bin/bash
+set -euo pipefail
 
-configs=$(ls -1 /etc/snapper/configs | grep disk)
+##################### FUNCTION DEFINITIONS #########################
+parse_args() {
+  # Set default values for options
+  configs=(/etc/snapper/configs/disk*)
+  disable=false
+  enable=false
 
-for config in ${configs}
-  do
-  echo "Disabling timeline snapshots for ${config}"
-  sed -i "s/TIMELINE_CREATE=\"yes\"/TIMELINE_CREATE=\"no\"/g" /etc/snapper/configs/${config}
-  mount=$(lsblk -o label,mountpoint | grep ${config} | awk '{print $2}')
-  snapshots=$(ls -1 ${mount}/.snapshots)
-  for snapshot in ${snapshots}
-    do
-    snapper -c ${config} delete ${snapshot} 2> /dev/null || echo "Config: ${config}; Snapshot: ${snapshot}; Mount: ${mount} ecnountered an error"
+  # Use getopt to parse command line arguments
+  ARGS=$(getopt -o hc:ed --long help,config:,enable,disable --name "$(basename "$0")" -- "$@")
+  # Get the command line arguments and assign them to variables according to their corresponding flags
+  # h: -> means the -h flag needs an argument
+  # l -> means the -l flag doesn't have an argument 
+  # "hc:e:d:" -> all the possible flags
+
+  eval set -- "${ARGS}"  # This sets positional parameters to the arguments that were used in getopt
+
+  while true; do
+    case "${1}" in  
+      -h|--help)
+        echo "Usage: $(basename "$0") [-c configs] [--enable | --disable]"
+        exit 0
+        ;;
+      -c|--configs)
+        configs=(${2})
+        shift 2
+        ;;
+      -d|--disable)
+        disable=true
+        shift 1
+        ;;
+      -e|--enable)
+        enable=true
+        shift 1
+        ;;
+      --)
+        shift
+        break
+        ;;
+      *)
+        echo "Error: Invalid option ${1}"
+        exit 1
+        ;;
+    esac
   done
-done
+}
+
+# Remove current snapper snapshots for all configs.
+remove_snapper_snapshots() {
+  local configs=("$@")
+  for config in "${configs[@]}"; do
+    label=$(basename "$config")
+    mountpoint=$(lsblk -o NAME,LABEL,MOUNTPOINT | awk -v label="$label" '$2 == label { print $3 }')
+    printf 'Removing current snapper snapshots for config %s...\nAt mountpoint: %s\n' "$label" "$mountpoint"
+    while IFS= read -r -d '' snapshot; do
+      printf 'Snapshot: %s\n' "$snapshot"
+      if ! snapper -c "$label" delete "$snapshot"; then
+        printf 'Error: could not delete snapshot %s\n' "$snapshot" >&2
+      fi
+    done < <(find "$mountpoint/.snapshots" -mindepth 1 -maxdepth 1 -type d -printf '%f\0' 2>/dev/null || true)
+  done
+}
+
+disable_snapper() {
+  local configs=("$@")
+  printf 'Disabling timeline snapshots for %s\n' "${configs[@]}"
+  sed -i "s/TIMELINE_CREATE=\"yes\"/TIMELINE_CREATE=\"no\"/g" "${configs[@]}"
+}
+
+enable_snapper() {
+  local configs=("$@")
+  printf 'Re-enable timeline snapshots for %s\n' "${configs[@]}"
+  sed -i "s/TIMELINE_CREATE=\"no\"/TIMELINE_CREATE=\"yes\"/g" "${configs[@]}"
+}
+
+# Call functions based on command line arguments.
+parse_args "$@"
+[[ $disable == true ]] && disable_snapper "${configs[@]}"
+remove_snapper_snapshots "${configs[@]}"
+[[ $enable == true ]] && enable_snapper "${configs[@]}"
